f0a90c4adef0cf338099cc72c320de490552b5da
Conny Sjögren <conny.sjogren@activesolution.se>
2020-03-13 09:41:57 +0100

    finish project sample

---
+++ Demo.NullableReferenceTypes.Project/Controllers/SampleController.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace Demo.NullableReferenceTypes.Project.Controllers
+{
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        private readonly SampleContext sampleContext;
+
+        public SampleController(SampleContext sampleContext)
+        {
+            this.sampleContext = sampleContext;
+        }
+
+        [HttpGet]
+        public Task<Company[]> Get()
+        {
+            return sampleContext.Companies
+                .Include(c => c.Employees)
+                .ToArrayAsync();
+        }
+
+        [HttpGet("employees/{id}")]
+        public Task<Person> Get(int id)
+        {
+            /*
+             EF Core's public API surface has not yet been annotated for nullability (the public API is "null-oblivious"), 
+             making it sometimes awkward to use when the NRT feature is turned on. 
+             This notably includes the async LINQ operators exposed by EF Core, such as FirstOrDefaultAsync. 
+             We plan to address this for the 5.0 release.
+             */
+            return sampleContext.People
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        [HttpGet("employees/{id}/boss")]
+        public Task<Person> GetBoss(int id)
+        {
+            return sampleContext.People
+                .Include(p => p.Boss)
+                .Where(p => p.Id == id)
+                .Select(p => p.Boss)
+                .FirstAsync();
+        }
+    }
+}
--- Demo.NullableReferenceTypes.Project/SampleContext.cs
+++ Demo.NullableReferenceTypes.Project/SampleContext.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 
 namespace Demo.NullableReferenceTypes.Project
 {
+    // https://docs.microsoft.com/en-us/ef/core/miscellaneous/nullable-reference-types#non-nullable-properties-and-initialization
     public class SampleContext : DbContext
     {
+        public SampleContext(DbContextOptions<SampleContext> options) : base (options)
+        {
+
+        }
         public DbSet<Person> People { get; set; }
         public DbSet<Company> Companies { get; set; }
     }
@@ -12,8 +17,11 @@ namespace Demo.NullableReferenceTypes.Project
     public class Person
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
         public Person Boss { get; set; }
+        public Company Company { get; set; }
     }
 
     public class Company
